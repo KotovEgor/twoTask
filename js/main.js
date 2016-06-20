aaa131579ecac3879b612fdcde8e1a499f068de2
Egor Kotov <trapatoni89@gmail.com>
2016-06-21 01:03:03 +0300

    Создание нового проекта

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Администратор on 21.06.2016.
+ */
+var Blank = Backbone.Model.extend({
+    defaults:{
+        person: "Котов Егор Владимирович",
+        job: "Программист",
+        ofWork: "МЭСИ",
+        phone: "89157029579",
+        date: "21.06.2016"
+
+    }
+
+})
