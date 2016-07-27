714444ffb47a24e5a0856e3709c3bb0385ea4a70
Котов Егор Владимирович <evkotov@tpk-tver.ru>
2016-07-27 17:07:53 +0300

    Добавил модель и представление

--- js/main.js
+++ js/main.js
@@ -1,21 +1,56 @@
-/**
- * Created by Администратор on 21.06.2016.
- */
-var Person = Backbone.Model.extend({
-    defaults:{
-        person: "Котов Егор Владимирович",
-        job: "Программист",
-        office: "МЭСИ",
-        phone: "89157029579",
-        date: "21.06.2016"
+$(function () {
 
-    },
 
-    validate: function( attrs ){
-        console.log(attrs);
-        if ( ! attrs.job ){
-            return "Введите должность";
+    var Person = Backbone.Model.extend({
+        defaults: {
+            name: "Котов Егор Владимирович",
+            job: "Программист",
+            office: "МЭСИ",
+            phone: "89157029579",
+            date: "21.06.2016"
+        },
+
+        validate: function (attrs) {
+            if ($.trim(attrs.job) == 0) {
+                return "Введите должность";
+            }
         }
-    }
+
+    })
+
+
+    var viewForm = Backbone.View.extend({
+        el:'#doc-form',
+        events: {
+            'keyup input': 'showName'
+        },
+        showName: function (e) {
+            newPerson.set($(e.currentTarget).attr('id'), $(e.currentTarget).val());
+        }
+    })
+
+    
+    var viewPerson = Backbone.View.extend({
+        initialize: function () {
+            this.model.on('change', this.render, this);
+        },
+        tagName: 'div',
+        className: 'doc',
+        template: _.template($('#template-name').html()),
+        render: function () {
+            this.$el.html( this.template( this.model.toJSON()) );
+            return this;
+        }
+    })
+
+    
+    
+    var newPerson = new Person;
+    var newForm = new viewForm;
+
+    var vivod = new viewPerson({model: newPerson});
+    
+    $('.right').html(vivod.render().el);
+
 
 })
